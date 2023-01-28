cbc75cd4c0a088081cba65690946df265e61e04a
Sergey Metelyov <metelew@gmail.com>
2023-01-28 12:22:47 +0300

    chore: fix

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ deps:
 	curl https://raw.githubusercontent.com/tesseract-ocr/tessdata_fast/65727574dfcd264acbb0c3e07860e4e9e9b22185/rus.traineddata -o rus.traineddata
 
 macos:
-	brew install pyenv curl tesseract
+	brew install pyenv pylint curl tesseract
--- img_recognizer/main.py
+++ img_recognizer/main.py
@@ -1,6 +1,6 @@
-import cv2
 import os
 import re
+import cv2
 import pytesseract
 
 image_name = "test.jpg"
@@ -11,28 +11,22 @@ split_words = ["Салаты", "Суп", "Второе", "Гарниры"]
 try:
     image = cv2.imread(image_name)
     print("Image opened successfully\n")
+    # Pre-processing
+    gray_text = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Recognize text using Tesseract
+    raw_text = pytesseract.image_to_string(gray_text, lang="rus", config="--psm 11")
+    raw_text = raw_text.replace('.', '')
+    raw_text = raw_text.replace('\n\n', ' ')
+    text = re.split("Салаты|Суп|Второе|Гарниры", raw_text)
+    menu = dict()
+    for i in split_words:
+        menu[i] = text[split_words.index(i) + 1]
+        menu[i] = menu[i].strip()
+    for i in menu:
+        menu[i] = re.split("руб|Руб", menu[i])
+        menu[i].pop(-1) # удаляет пустой последний элемент
+    print(menu["Суп"])
+
 except Exception as e:
     print(f"Error: {e}")
     print(f"No image found: {image_name}")
-
-# Pre-processing
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# Recognize text using Tesseract
-raw_text = pytesseract.image_to_string(gray, lang="rus", config="--psm 11")
-raw_text = raw_text.replace('.', '')
-raw_text = raw_text.replace('\n\n', ' ')
-# raw_text = raw_text.lower()
-
-text = re.split("Салаты|Суп|Второе|Гарниры", raw_text)
-
-menu = dict()
-for i in split_words:
-    menu[i] = text[split_words.index(i) + 1]
-    menu[i] = menu[i].strip()
-    # print(menu[i])
-for i in menu:
-    menu[i] = re.split("руб|Руб", menu[i])
-    # for j in menu[i]:
-    #     j = j.strip()
-    menu[i].pop(-1) # удаляет пустой последний элемент
-print(menu["Суп"])
