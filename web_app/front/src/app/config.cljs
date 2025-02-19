b74ea285fc00ae633aa1c93ff58ae7263b400a0e
k3nj1g <k3nj1g@gmail.com>
2025-02-19 13:59:57 +0300

    fix(front): fix build

--- web_app/front/src/app/config.cljs
+++ web_app/front/src/app/config.cljs
@@ -3,9 +3,12 @@
 (def debug?
   ^boolean goog.DEBUG)
 
-(goog-define API_URL "http://localhost:8080")
+(println "goog.API_URL" goog.API_URL)
+(println "API_URL" API_URL)
+(println "app.config.API_URL" app.config.API_URL)
+
 
 (def config
-  {:api-url API_URL})
+  {:api-url goog.API_URL})
 
 (def api-url (:api-url config))
